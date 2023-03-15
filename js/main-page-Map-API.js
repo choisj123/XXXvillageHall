9786c743f54254db96ba6599d3f460141f79e80d
ryu <mrryu555@gmail.com>
2023-03-15 11:35:59 +0900

    add profile img

--- js/main-page-Map-API.js
+++ js/main-page-Map-API.js
@@ -1,7 +1,3 @@
-
-
-
-
 var mapContainer = document.getElementById('map'),
     mapOption = { 
         center: new kakao.maps.LatLng(37.566826, 126.9786567), 
@@ -18,11 +14,13 @@ var markers = [];
 var markersData = [
   {
     name: "정훈쓰",
-    location: new kakao.maps.LatLng(37.549264, 124.913598),
+    location: new kakao.maps.LatLng(37.549264, 126.94500),
     title : "글제목1",
     createAt : "2023-03-08 10:00:00",
     content: "살려주세요",
-    category: "이슈"
+    like : "500",
+    category:"친목",
+    photoUrl: 'https://media.tenor.com/91NRJO-HG0IAAAAi/%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94%EB%A1%9C%EC%95%84%EC%BD%98-%EB%AA%A8%EC%BD%94%EC%BD%94.gif' 
   },
   {
     name: "수진쓰",
@@ -30,7 +28,9 @@ var markersData = [
     title : "글제목2",
     createAt : "2023-03-08 11:00:00",
     content: "시간 너무 안가요",
-    category:"명소" 
+    like : "180",
+    category:"이슈",
+    photoUrl: 'https://media.tenor.com/hXlsX_G7ma0AAAAi/%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94%EB%A1%9C%EC%95%84%EC%BD%98-%EB%AA%A8%EC%BD%94%EC%BD%94.gif'
   },
   {
     name: "정윤쓰",
@@ -38,15 +38,19 @@ var markersData = [
     title : "글제목3",
     createAt: "2023-03-08 12:40:00",
     content: "자고싶다",
-    category:"취미"
+    like : "260",
+    category:"취미",
+    photoUrl: 'https://media.tenor.com/lSU2BNd-fyYAAAAj/%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94%EB%A1%9C%EC%95%84%EC%BD%98-%EB%AA%A8%EC%BD%94%EC%BD%94.gif'
   },
   {
     name: "동준쓰",
     location: new kakao.maps.LatLng(37.575868, 126.976781),
     title : "글제목4",
     createAt: "2023-03-08 22:10:03",
-    content: "집에가고싶어요",
-    category:"친목"
+    content: "집에안가고싶어요",
+    like : "540",
+    category:"친목",
+    photoUrl: 'https://media.tenor.com/HaSLxXI5y1oAAAAi/%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94%EB%A1%9C%EC%95%84%EC%BD%98-%EB%AA%A8%EC%BD%94%EC%BD%94.gif'
   },
   {
     name: "민성쓰",
@@ -54,7 +58,9 @@ var markersData = [
     title : "글제목5",
     createAt: "2023-03-08 20:50:00",
     content: "안녕하세요",
-    category: "추천"
+    like : "200",
+    category: "추천",
+    photoUrl: 'https://media.tenor.com/7bS_ec1TjfEAAAAi/%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94%EB%A1%9C%EC%95%84%EC%BD%98-%EB%AA%A8%EC%BD%94%EC%BD%94.gif'
   }
 ];
 
@@ -74,45 +80,22 @@ for (var i = 0; i < markersData.length; i++) {
       return function() {
           var infowindow = new kakao.maps.InfoWindow({
               content: 
-              '<div class="map-container">' +
-              '<!-- header --> ' +
-              '<div class="info">' +
-              '  <div class="title">' + 
-                  markersData[i].name +
-              '  </div>' +
-              '    <div class="close">' + markersData[i].createAt + '</div>' +
+              '<div class="infowindow-container">' +
+              '<div class="infowindow-header">' + 
+                '<div class="inwi-left"><img src="'+ markersData[i].photoUrl +'width="58" height="58"></div>' +
+                '<div class="inwi-right">' +
+                 ' <div>' + markersData[i].name + '</div>' +
+                 ' <div class="time">' + markersData[i].createAt + '</div>' + 
+                 ' <div> #' +  markersData[i].category + '</div>' +
+                '</div>' +
               '</div>' +
-              '<!-- nav -->' +
-              '<div class="nav-section">' +
-              '  <div class="left">' +
-              '    <a href="#">[서초구]</a>' +
-              '    <a href="#">' + markersData[i].category + '</a>' +
-              '  </div>' +
-              '  <div class="right">' +
-              '    <span>조회수 400</span>' +
-              '    <span>좋아요 400</span>' +
-              '  </div>' +
-              '</div>' +
-              '<!-- main-content -->' +
-              '<div class="main">' +
-              '  <!-- aside -->' + 
-              '  <div></div>' +
-              '  <!-- main -->'+
-              '  <div class="main-content">'+
-              '    <!-- 글제목 -->'+
-              '    <div>'+
-              '      <p>' + markersData[i].title +
-              '</p>'+
-              '    </div>'+
-              '    <!-- 작성글 -->'+
-              '    <div>'+
-              '      <p>' + markersData[i].content + '</p>'+
-              '    </div>'+
-              '  </div>'+
-              '  <!-- aside -->'+
-              '  <div></div>'+
+             ' <div class="infowindow-content">' +
+              '<div class="info-title">' + markersData[i].title + '</div>' +
+              '<div class="info-content">' + markersData[i].content + '</div>' +
               '</div>'+
-          ' </div>',
+              '<div class="infowindow-footer">좋아요 : ' + markersData[i].like + '</div>'+
+            '</div>'
+            
           });
           infowindow.open(map, marker);
           map.addListener('click',  function() {

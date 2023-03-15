cd18d1eac281665dfcf269ae534dd04d4a857b50
Apsalus <allnight724@gmail.com>
2023-03-15 14:00:08 +0900

    Update FAQ-page.js

--- js/FAQ-page.js
+++ js/FAQ-page.js
@@ -2,6 +2,17 @@ const tabButtons = document.querySelectorAll(".category-article a");
 
 const tabContents = document.querySelectorAll(".FAQ-board");
 
+tabButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    // 탭 버튼 색상과 폰트 굵기 변경
+    tabButtons.forEach(function(button) {
+      button.classList.remove("active");
+      button.style.color = "black";
+      button.style.fontWeight = "normal";
+    });
+    this.classList.add("active");
+    this.style.color = "white";
+    this.style.fontWeight = "bold";
 
 
 const targetContentId = this.getAttribute("data-tab");
